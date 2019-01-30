a137aa435c543bd4ad3cb648dbe3c3bac7b2af51
emmaTu <emma.n.tuovinen@gmail.com>
2019-01-30 15:26:56 +0200

    edited according to PR comments

--- front/src/Serviceclient.js
+++ front/src/Serviceclient.js
@@ -3,50 +3,47 @@ import Axios from "axios";
 const API = "http://localhost:5000/api/";
 
 export function CreateUser(userData, callback) {
-    let newUser = {firstName: userData.firstName, 
-        lastName: userData.lastName, 
-        email: userData.email, 
-        role: userData.role,
-        phoneNumber: userData.phoneNumber,
-        description: userData.description
-    }
-    Axios.post(API + "Users/", newUser).then(response => {
-        console.dir(response)
-    });
+  let newUser = {
+    firstName: userData.firstName,
+    lastName: userData.lastName,
+    email: userData.email,
+    role: userData.role,
+    phoneNumber: userData.phoneNumber,
+    description: userData.description
+  };
+  Axios.post(API + "Users/", newUser).then(response => {
+    callback(response);
+  });
 }
 
 export function GetAllConsultants(callback) {
-    Axios.get(API + "Users/consultants").then(response => {
-        callback(response.data);
-    });
+  Axios.get(API + "Users/consultants").then(response => {
+    callback(response.data);
+  });
 }
 
 export function GetAllUsers(callback) {
-    Axios.get(API + "Users/").then(response => {
-        callback(response.data);
-    });
+  Axios.get(API + "Users/").then(response => {
+    callback(response.data);
+  });
 }
 
 export function GetConsultantInfo(userId, callback) {
-    Axios.get(API + "Users/" + userId).then(response => {
-        callback(response.data)
-    });
+  Axios.get(API + "Users/" + userId).then(response => {
+    callback(response.data);
+  });
 }
 
-export function DeleteUser(userId) {
-    Axios.delete(API + "Users/" + userId).then(response => {
-        console.dir(response)
-    });
+export function DeleteUser(userId, callback) {
+  Axios.delete(API + "Users/" + userId).then(response => {
+    callback(response);
+  });
 }
 
-export function EditProfile(userId, user) {
-    Axios.put(API + "Users/" + userId, user).then(response => {
-        console.dir(response)
-    });
-}
-
-export function GetOpportunities(callback) {
-    // here we list all job opportunities
+export function EditProfile(userId, user, callback) {
+  Axios.put(API + "Users/" + userId, user).then(response => {
+    callback(response);
+  });
 }
 
 
