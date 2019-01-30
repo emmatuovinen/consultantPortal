afa8a74bec7d137a784fda75530a1255dd8ef2dc
emmaTu <emma.n.tuovinen@gmail.com>
2019-01-30 14:36:09 +0200

    serviceclient: added delete, edit and create user

--- front/src/Serviceclient.js
+++ front/src/Serviceclient.js
@@ -1,27 +1,53 @@
 import Axios from "axios";
 
-const localhost = "http://localhost:5000";
+const API = "http://localhost:5000/api/";
+
+export function CreateUser(userData, callback) {
+    let newUser = {firstName: userData.firstName, 
+        lastName: userData.lastName, 
+        email: userData.email, 
+        role: userData.role,
+        phoneNumber: userData.phoneNumber,
+        description: userData.description
+    }
+    Axios.post(API + "Users/", newUser).then(response => {
+        console.dir(response)
+    });
+}
 
 export function GetAllConsultants(callback) {
-    Axios.get(localhost + "/api/Users/consultants").then(response => {
+    Axios.get(API + "Users/consultants").then(response => {
         callback(response.data);
-    })
+    });
 }
 
 export function GetAllUsers(callback) {
-    Axios.get(localhost + "/api/Users").then(response => {
+    Axios.get(API + "Users/").then(response => {
         callback(response.data);
-    })
+    });
 }
 
 export function GetConsultantInfo(userId, callback) {
-    Axios.get(localhost + "/api/Users/" + userId).then(response => {
+    Axios.get(API + "Users/" + userId).then(response => {
         callback(response.data)
-    })
+    });
 }
 
 export function DeleteUser(userId) {
-    Axios.delete(localhost)
+    Axios.delete(API + "Users/" + userId).then(response => {
+        console.dir(response)
+    });
 }
+
+export function EditProfile(userId, user) {
+    Axios.put(API + "Users/" + userId, user).then(response => {
+        console.dir(response)
+    });
+}
+
+export function GetOpportunities(callback) {
+    // here we list all job opportunities
+}
+
 
 export default function() {}
