d850894c935f38a5bd79981ca90dd1806bb07858
esle <essi.lehtola@hotmail.com>
2019-01-31 14:37:37 +0200

    Profile default details created

--- front/src/Containers/UserProfile.js
+++ front/src/Containers/UserProfile.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import {
+    Button, Form, FormGroup, Label, Input,
+    Table,
+} from 'reactstrap';
 import { GetConsultantInfo } from '../ServiceClient'
 
 class UserProfile extends Component {
     state = {
-        userId: '20',
+        userId: '15',
         user: {},
         editingDisabled: true,
-        firstName: ''
+        userIsConsultant: true,
     }
 
     componentDidMount() {
         GetConsultantInfo(this.state.userId, callback => {
 
-            if (callback.status == 200) {
+            if (callback.status === 200) {
                 this.setState({ user: callback.data });
                 console.log('stateen laitettu user', this.state.user)
             } else {
@@ -51,6 +54,14 @@ class UserProfile extends Component {
                 copyOfUser.description = event.target.value;
                 this.setState({ user: copyOfUser });
                 break;
+            case 'role':
+                console.log('radio button painettu', event.target)
+                copyOfUser.role = event.target.value;
+                this.setState({
+                    userIsConsultant: !this.state.userIsConsultant,
+                    user: copyOfUser
+                });
+                break;
             default:
                 break;
         }
@@ -62,35 +73,35 @@ class UserProfile extends Component {
             <Form>
                 <FormGroup controlid="firstName">
                     <Label for="firstName">First name</Label>
-                    <Input onChange={this.handleChange} disabled={this.state.editingDisabled} type="text" name="firstName" id="firstName" value={user.firstName} />
+                    <Input onChange={this.handleChange} type="text" name="firstName" id="firstName" value={user.firstName} />
                 </FormGroup>
                 <FormGroup controlid="lastName">
                     <Label for="lastName">Last name</Label>
-                    <Input onChange={this.handleChange} disabled={this.state.editingDisabled} type="text" name="lastName" id="lastName" value={user.lastName} />
+                    <Input onChange={this.handleChange} type="text" name="lastName" id="lastName" value={user.lastName} />
                 </FormGroup>
                 <FormGroup controlid="email">
                     <Label for="email">Email</Label>
-                    <Input onChange={this.handleChange} disabled={this.state.editingDisabled} type="email" name="email" id="email" value={user.email} />
+                    <Input onChange={this.handleChange} type="email" name="email" id="email" value={user.email} />
                 </FormGroup>
                 <FormGroup controlid="phoneNumber">
                     <Label for="phoneNumber">Phone</Label>
-                    <Input onChange={this.handleChange} disabled={this.state.editingDisabled} type="number" name="phoneNumber" id="phoneNumber" value={user.phoneNumber} />
+                    <Input onChange={this.handleChange} type="number" name="phoneNumber" id="phoneNumber" value={user.phoneNumber} />
                 </FormGroup>
                 <FormGroup controlid="description">
                     <Label for="description">Description</Label>
-                    <Input onChange={this.handleChange} disabled={this.state.editingDisabled} type="text" name="description" id="description" value={user.description} />
+                    <Input onChange={this.handleChange} type="text" name="description" id="description" value={user.description} />
                 </FormGroup>
                 <FormGroup tag="fieldset">
                     <FormGroup controlid="role">
                         <FormGroup check>
                             <Label check>
-                                <Input disabled={this.state.editingDisabled} type="radio" name="role" defaultChecked />{' '}
+                                <Input onChange={this.handleChange} type="radio" id="role" name="role" value="Consultant" checked={this.state.userIsConsultant} />{' '}
                                 Consultant
                                 </Label>
                         </FormGroup>
                         <FormGroup check>
                             <Label check>
-                                <Input disabled={this.state.editingDisabled} type="radio" name="role" />{' '}
+                                <Input onChange={this.handleChange} type="radio" id="role" name="role" value="AM" checked={!this.state.userIsConsultant} />{' '}
                                 AM
                                 </Label>
                         </FormGroup>
@@ -100,10 +111,44 @@ class UserProfile extends Component {
         )
     }
 
+    renderUserDetails() {
+        let user = this.state.user;
+        return (
+            <Table>
+                <tbody>
+                    <tr>
+                        <th scope="row"><Label>First name: </Label></th>
+                        <td><Label>{user.firstName}</Label></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><Label>Last name: </Label></th>
+                        <td><Label>{user.lastName}</Label></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><Label>Email: </Label></th>
+                        <td><Label>{user.email}</Label></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><Label>Phone number: </Label></th>
+                        <td><Label>{user.phoneNumber}</Label></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><Label>Description: </Label></th>
+                        <td><Label>{user.description}</Label></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><Label>Role: </Label></th>
+                        <td><Label>{user.role}</Label></td>
+                    </tr>
+                </tbody>
+            </Table>
+        )
+    }
+
     render() {
         return (
             <div className='container'>
-                {this.state.editingDisabled ? <div>Data in none editing mode</div> : this.renderForm()}
+                {this.state.editingDisabled ? this.renderUserDetails() : this.renderForm()}
                 <Button onClick={this.edit}>{this.state.editingDisabled ? 'Edit' : 'Save'}</Button>
             </div>
         );
