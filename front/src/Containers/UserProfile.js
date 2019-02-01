2b5bdd4a777a846e1cd4126444a02744c0c90b89
esle <essi.lehtola@hotmail.com>
2019-02-01 11:55:16 +0200

    some refactoring after code review

--- front/src/Containers/UserProfile.js
+++ front/src/Containers/UserProfile.js
@@ -1,46 +1,53 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Button, Container } from 'reactstrap';
 
 import { GetConsultantInfo, EditProfile } from '../ServiceClient'
 import UserProfileForm from '../Components/UserProfileForm';
 import UserProfileDetails from '../Components/UserProfileDetails';
 
-const USER_ID = '1';
+const USER_ID = '1'; // hard coded userId for demo purposes
 
 export default class UserProfile extends Component {
     state = {
         userId: USER_ID,
-        user: {},
+        user: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phoneNumber: '',
+            description: '',
+            role: ''
+        },
         isEditing: false,
         userIsConsultant: true,
     }
 
     componentDidMount() {
-        GetConsultantInfo(this.state.userId, callback => {
-            if (callback.status === 200) {
-                let user = callback.data;
+        GetConsultantInfo(this.state.userId, response => {
+            if (response.status === 200) {
+                let user = response.data;
                 let userIsConsultant = (user.role === 'Consultant');
                 this.setState({ user, userIsConsultant });
             } else {
-                console.log('error', callback.status);
+                console.log('error', response.status);
                 // some kind of redirect to an error page?
             }
         });
     }
 
     editMode = (btn) => {
-        this.setState({ isEditing: !this.state.isEditing });
         if (btn.target.value === 'Save') {
-            EditProfile(this.state.userId, this.state.user, callback => {
-                if (callback.status === 200) {
-                    console.log('success', callback.status);
+            EditProfile(this.state.userId, this.state.user, response => {
+                if (response.status === 200) {
+                    console.log('success', response.status);
                     // some kind of 'save successfull' message for the user?
                 } else {
-                    console.log('error', callback.status);
+                    console.log('error', response.status);
                     // some kind of redirect to an error page?
                 }
             });
         }
+        this.setState({ isEditing: !this.state.isEditing });
     }
 
     handleChange = event => {
@@ -48,34 +55,29 @@ export default class UserProfile extends Component {
         switch (event.target.id) {
             case 'firstName':
                 copyOfUser.firstName = event.target.value;
-                this.setState({ user: copyOfUser });
                 break;
             case 'lastName':
                 copyOfUser.lastName = event.target.value;
-                this.setState({ user: copyOfUser });
                 break;
             case 'email':
                 copyOfUser.email = event.target.value;
-                this.setState({ user: copyOfUser });
                 break;
             case 'phoneNumber':
                 copyOfUser.phoneNumber = event.target.value;
-                this.setState({ user: copyOfUser });
                 break;
             case 'description':
                 copyOfUser.description = event.target.value;
-                this.setState({ user: copyOfUser });
                 break;
             case 'role':
                 copyOfUser.role = event.target.value;
                 this.setState({
                     userIsConsultant: !this.state.userIsConsultant,
-                    user: copyOfUser
                 });
                 break;
             default:
                 break;
         }
+        this.setState({ user: copyOfUser });
     }
 
     renderUserProfileForm() {
@@ -93,11 +95,11 @@ export default class UserProfile extends Component {
     render() {
         let buttonText = this.state.isEditing ? 'Save' : 'Edit';
         return (
-            <div className='container'>
+            <Container>
                 <h2>User profile</h2>
                 {this.state.isEditing ? this.renderUserProfileForm() : this.renderUserProfileDetails()}
                 <Button onClick={this.editMode} value={buttonText}>{buttonText}</Button>
-            </div>
+            </Container>
         );
     }
 }
