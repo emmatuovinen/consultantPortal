870c9276b2d14e19f7befbc8eb54777066993c49
ilkkajok <ilkka.jokela@gmail.com>
2019-04-15 12:33:21 +0300

    added azure ad auth to backend

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ using WebApi.Repository;
 
 namespace WebApi.Controllers
 {
+    [Authorize]
     [Produces("application/json")]
     [Route("api/[Controller]")]
     public class UsersController : ControllerBase
