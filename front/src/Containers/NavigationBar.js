0bd4adfebef3c63a3cb0187862d70b0eedf6d873
esle <essi.lehtola@hotmail.com>
2019-04-29 15:28:47 +0300

    logical error fixed + made sure changing props get into state

--- front/src/Containers/NavigationBar.js
+++ front/src/Containers/NavigationBar.js
@@ -10,8 +10,12 @@ export default class NavigationBar extends Component {
     this.toggle = this.toggle.bind(this);
     this.state = {
       isOpen: false,
-      userIsAW: this.props.childProps.role !== "AW"
+      userIsAW: this.props.childProps.role === "AW"
     };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return state.userIsAW = props.childProps.role === "AW";
   }
 
   toggle() {
@@ -20,46 +24,51 @@ export default class NavigationBar extends Component {
 
   render() {
     return (
-      <Navbar light expand="md">
-        <NavbarBrand href="/">
-          <img
-            src={logo}
-            style={{ width: 150, height: 35, marginTop: 1 }}
-            alt="AW-logo" />
-        </NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar className="collapsed-items">
-          <Nav className="ml-auto" navbar>
-            {this.props.childProps.isAuthenticated === true
-              ? <>
-                <NavItem>
-                  <NavLink href="/consultants">Consultants</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/positions">Positions</NavLink>
-                </NavItem>
-                {this.state.userIsAW &&
-                  <NavItem>
-                    <NavLink href="/positions/add">Add position</NavLink>
-                  </NavItem>
-                }
-                <NavItem>
-                  <NavLink href="/profile">My Profile</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    style={{ cursor: 'pointer' }}
-                    onClick={this.props.childProps.logout}>Logout</NavLink>
-                </NavItem>
-              </>
-              : <NavItem>
-                <NavLink
-                  style={{ cursor: 'pointer' }}
-                  onClick={this.props.childProps.login}>Login</NavLink>
-              </NavItem>}
-          </Nav>
-        </Collapse>
-      </Navbar>
+      <>
+        {
+          this.props.childProps.role !== "" &&
+          <Navbar light expand="md">
+            <NavbarBrand href="/">
+              <img
+                src={logo}
+                style={{ width: 150, height: 35, marginTop: 1 }}
+                alt="AW-logo" />
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar className="collapsed-items">
+              <Nav className="ml-auto" navbar>
+                {this.props.childProps.isAuthenticated === true
+                  ? <>
+                    <NavItem>
+                      <NavLink href="/consultants">Consultants</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink href="/positions">Positions</NavLink>
+                    </NavItem>
+                    {this.state.userIsAW &&
+                      <NavItem>
+                        <NavLink href="/positions/add">Add position</NavLink>
+                      </NavItem>
+                    }
+                    <NavItem>
+                      <NavLink href="/profile">My Profile</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        style={{ cursor: 'pointer' }}
+                        onClick={this.props.childProps.logout}>Logout</NavLink>
+                    </NavItem>
+                  </>
+                  : <NavItem>
+                    <NavLink
+                      style={{ cursor: 'pointer' }}
+                      onClick={this.props.childProps.login}>Login</NavLink>
+                  </NavItem>}
+              </Nav>
+            </Collapse>
+          </Navbar>
+        }
+      </>
     );
   }
 }
