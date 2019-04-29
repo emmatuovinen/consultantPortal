b2e8aef7785b40c0d01912c08d9471c82e09c41a
esle <essi.lehtola@hotmail.com>
2019-04-29 13:19:39 +0300

    changed cursor style for login menu item

--- front/src/Containers/NavigationBar.js
+++ front/src/Containers/NavigationBar.js
@@ -20,44 +20,46 @@ export default class NavigationBar extends Component {
 
   render() {
     return (
-        <Navbar light expand="md">
-          <NavbarBrand href="/">
-            <img
-              src={logo}
-              style={{ width: 150, height: 35, marginTop: 1 }}
-              alt="AW-logo" />
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar className="collapsed-items">
-            <Nav className="ml-auto" navbar>
-              {this.props.childProps.isAuthenticated === true
-                ? <>
+      <Navbar light expand="md">
+        <NavbarBrand href="/">
+          <img
+            src={logo}
+            style={{ width: 150, height: 35, marginTop: 1 }}
+            alt="AW-logo" />
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar className="collapsed-items">
+          <Nav className="ml-auto" navbar>
+            {this.props.childProps.isAuthenticated === true
+              ? <>
+                <NavItem>
+                  <NavLink href="/consultants">Consultants</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/positions">Positions</NavLink>
+                </NavItem>
+                {this.state.userIsAW &&
                   <NavItem>
-                    <NavLink href="/consultants">Consultants</NavLink>
+                    <NavLink href="/positions/add">Add position</NavLink>
                   </NavItem>
-                  <NavItem>
-                    <NavLink href="/positions">Positions</NavLink>
-                  </NavItem>
-                  {this.state.userIsAW &&
-                    <NavItem>
-                      <NavLink href="/positions/add">Add position</NavLink>
-                    </NavItem>
-                  }
-                  <NavItem>
-                    <NavLink href="/profile">My Profile</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      style={{ cursor: 'pointer' }}
-                      onClick={this.props.childProps.logout}>Logout</NavLink>
-                  </NavItem>
-                </>
-                : <NavItem>
-                  <NavLink onClick={this.props.childProps.login}>Login</NavLink>
-                </NavItem>}
-            </Nav>
-          </Collapse>
-        </Navbar>
+                }
+                <NavItem>
+                  <NavLink href="/profile">My Profile</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    style={{ cursor: 'pointer' }}
+                    onClick={this.props.childProps.logout}>Logout</NavLink>
+                </NavItem>
+              </>
+              : <NavItem>
+                <NavLink
+                  style={{ cursor: 'pointer' }}
+                  onClick={this.props.childProps.login}>Login</NavLink>
+              </NavItem>}
+          </Nav>
+        </Collapse>
+      </Navbar>
     );
   }
 }
